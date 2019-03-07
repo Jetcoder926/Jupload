fbae6ee9a6ecba0adc7a134c90c8d29159f56b14
jetcoder <jetcoder@163.com>
2019-03-07 23:40:24 +0800

    fix config file

--- src/config.php
+++ src/config.php
@@ -1,12 +1,12 @@
 <?php
 return [
-    'jupload'=>[
+
             'store_passage'=>'local',
-            'image_local'=>storage_path('images'),
+            'image_local'=>'images/',
             'image_size_limit'=>2 * 1024,
             'file_size_limit'=>50 * 1024,
             'thumb_size_limit'=>'400,300',
             'file_ext'=>'doc,docx,xls,xlsx,ppt,pptx,pdf,wps,txt,rar,zip,gz,bz2,7z',
             'image_ext'=>'gif,jpg,jpeg,bmp,png',
-    ]
+
 ];
